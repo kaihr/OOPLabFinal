e62a923bcc02e27c8317aa7d64ac0e56bc072cf0
ndhuy21 <huytinhoc21@gmail.com>
2021-12-31 16:29:29 +0700

    add moveToString

--- SFMLStuff/MovingPawnState.cpp
+++ SFMLStuff/MovingPawnState.cpp
@@ -5,25 +5,27 @@ GameState* MovingPawnState::handleInput(const sf::Event& event, ChessGame& owner
 {
 	if (event.type == sf::Event::MouseButtonPressed) {
 		sf::Vector2i cell = Utility::getCell(sf::Mouse::getPosition(owner._window));
-	
+
 		if (Utility::inBoard(cell.x, cell.y)) {
 
 			if (!owner._pieces[cell.x][cell.y]
 				&& (cell.x == _row + _positiveDirection)
 				&& (cell.y == _col + 1 || cell.y == _col - 1)) {
-				
+
 				// Another pawn of different color
 				if (owner._pieces[_row][cell.y]
 					&& owner._pieces[_row][cell.y]->isWhite() != owner._currentChosen->isWhite()
 					&& owner._pieces[_row][cell.y]->type() == Piece::Type::PAWN
 					&& owner._preChosen == owner._pieces[_row][cell.y]) {
-					
+
 					delete owner._pieces[_row][cell.y];
 					owner._pieces[_row][cell.y] = NULL;
 
 					owner._pieces[_row][_col] = NULL;
 					owner._pieces[cell.x][cell.y] = owner._currentChosen;
 					owner._currentChosen->setPos(cell.x, cell.y);
+
+					std::cout << owner._currentChosen->moveToString(_row, _col, cell.x, cell.y) << '\n';
 
 					acceptMove(owner);
 					return new NullState();
@@ -42,6 +44,8 @@ GameState* MovingPawnState::handleInput(const sf::Event& event, ChessGame& owner
 				owner._pieces[cell.x][cell.y] = owner._currentChosen;
 				owner._currentChosen->setPos(cell.x, cell.y);
 
+				std::cout << owner._currentChosen->moveToString(_row, _col, cell.x, cell.y) << '\n';
+
 				acceptMove(owner);
 				owner._preChosen->switchEnPassant();
 				return new NullState();
@@ -55,6 +59,8 @@ GameState* MovingPawnState::handleInput(const sf::Event& event, ChessGame& owner
 				owner._pieces[_row][_col] = NULL;
 				owner._pieces[cell.x][cell.y] = owner._currentChosen;
 				owner._currentChosen->setPos(cell.x, cell.y);
+
+				std::cout << owner._currentChosen->moveToString(_row, _col, cell.x, cell.y) << '\n';
 
 				return new PromotionState(cell.x, cell.y);
 			}
